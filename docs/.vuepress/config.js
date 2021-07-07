2552d284bb243526dbb577191ee65b305667a174
Carl Alexander <carlalexander@gmail.com>
2021-07-07 14:17:41 -0400

    add title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,6 +14,7 @@ module.exports = {
     }
   },
   sidebar: true,
+  title: 'Ymir',
   head: [
     ['link', { rel: 'apple-touch-icon', sizes:'180x180', href: '/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes:'16x16', href: '/favicon-16x16.png' }],
